6575ae7b456317fa2f638085bfcae843661d560c
ImVinzzzz <vricci75@gmail.com>
2026-07-10 20:59:35 +0200

    Update BookGenerator.tsx

--- src/pages/BookGenerator.tsx
+++ src/pages/BookGenerator.tsx
@@ -451,9 +451,7 @@ export default function BookGenerator(): ReactElement {
                       <option value="02">02</option>
                       <option value="03">03</option>
                       <option value="04">04</option>
-                      <option value="05">05</option>
-                      <option value="06">06</option>
-                      <option value="07">07</option>
+                      <option value="05">07</option>
                     </select>
                   </div>
                   <div>
